db74b5a208dec80c1e52b8ff6e2a072410356e24
Beneš Jan (266907) <266907@vutbr.cz>
2025-04-28 10:20:24 +0200

    reseni

--- fibo.py
+++ fibo.py
@@ -1,6 +1,14 @@
+def recursive_nth_fibo(position):
+    if position == 0:
+        return 0
+    elif position == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(position -1) + recursive_nth_fibo(position -2)
+
 
 def main():
-    pass
+    print(recursive_nth_fibo(12))
 
 
 if __name__ == "__main__":
