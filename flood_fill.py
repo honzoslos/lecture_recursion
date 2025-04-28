25309153e30e9fe5409d797cec51abee196a4660
Beneš Jan (266907) <266907@vutbr.cz>
2025-04-28 10:46:56 +0200

    reseni

--- flood_fill.py
+++ flood_fill.py
@@ -1,5 +1,20 @@
 import matplotlib.pyplot as plt
 
+def flood_fill(picture,x,y):
+    """hodnoty x u array jsou v tomto pripade hodnoty uvnitr vnoreneho seznamu(, kdy se nejdna o seznamy,
+    ovsem o matice a indexy matic - rakdu a sloupcu, ovsem zatim pro zjednoduseni)
+       a pozice vnoreneho seznamu v seznamu odpovida hodnote y"""
+    if x < 0 or y < 0:
+        return picture
+    if picture[y[x]] == 0 or picture[y[x]] == 2:
+        return
+    if picture[y[x]] == 1:
+        picture[y[x]] = 2
+        flood_fill(picture, x+1, y)
+        flood_fill(picture, x-1, y)
+        flood_fill(picture, x, y+1)
+        flood_fill(picture, x, y-1)
+
 
 def main():
     img = plt.imread("files/img0.png")[:, :, 0]
@@ -7,6 +22,8 @@ def main():
     # img = plt.imread("files/img2.png")[:, :, 0]
 
     # img = floodfill(img, 0, 0)
+    img = flood_fill(img,0,0)
+
 
     plt.imshow(img, cmap="gray")
     plt.show(block=False)
